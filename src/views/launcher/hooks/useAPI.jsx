2a063e4d013aa67e6a8f8f02607af371ad7c91f4
red365 <25439820+red365@users.noreply.github.com>
2022-12-01 13:08:17 +0000

    Fix project stories list not being rendered in launcher

--- src/views/launcher/hooks/useAPI.jsx
+++ src/views/launcher/hooks/useAPI.jsx
@@ -11,7 +11,7 @@ const useAPI = (selectedProject = undefined) => {
   useEffect(() => {
     fetch(`/get/project/${selectedProject}/slideshows/`).then(res => res.json()).then(res => setProjectSlideshows(res));
     fetch(`/get/project/${selectedProject}/video/local/`).then(res => res.json()).then(res => setProjectLocalVideos(res));
-    fetch(`/get/project/${selectedProject}/stories/`).then(res => res.json()).then(res => setProjectStories({ stories: res }));
+    fetch(`/get/project/${selectedProject}/stories/`).then(res => res.json()).then(res => setProjectStories(res));
   }, [selectedProject])
 
   return { projects, selectedProject, projectSlideshows, projectLocalVideos, projectStories };
